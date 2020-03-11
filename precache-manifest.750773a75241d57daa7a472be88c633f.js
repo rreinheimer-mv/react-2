8805ab18672213464cc267d0b43d405f7b16728a
Rodrigo Reinheimer <rreinheimer@crossview.com>
2020-03-10 18:01:22 -0700

    Updates

---
+++ precache-manifest.750773a75241d57daa7a472be88c633f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb18d31600de0b30b4fa40a55ba70904",
+    "url": "/react-2/index.html"
+  },
+  {
+    "revision": "d19a01731b6742e32b58",
+    "url": "/react-2/static/css/main.576c9657.chunk.css"
+  },
+  {
+    "revision": "7bec8c25e89fb406b62e",
+    "url": "/react-2/static/js/2.624448eb.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-2/static/js/2.624448eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d19a01731b6742e32b58",
+    "url": "/react-2/static/js/main.9cf1c1bd.chunk.js"
+  },
+  {
+    "revision": "684d2566ff95be97c0e8",
+    "url": "/react-2/static/js/runtime-main.26f6cd13.js"
+  }
+]);
